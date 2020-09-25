ff2e03b3defe39f80e1e61c3ae2b68f1558a1f37
teago83 <thyago.pereira@faculdadecesusc.edu.br>
2020-09-25 10:46:39 -0300

    Já é possível pegar o input do usuário. Só falta passá-lo para outras telas.

--- Projeto/app/src/main/java/com/example/n1/MainActivity.java
+++ Projeto/app/src/main/java/com/example/n1/MainActivity.java
@@ -6,22 +6,36 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    String Name;
+    EditText InputName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        InputName = (EditText) findViewById(R.id.nome);
+
         Button BotaoSegundaTela = (Button) findViewById(R.id.BotaoSegundaTela);
         BotaoSegundaTela.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Name = InputName.getText().toString();
                 Intent IrSegundaTela = new Intent(MainActivity.this, ActivityNotas.class);
                 // adicione o esquema de catar o nome da pessoa e passar para a intent
                 startActivity(IrSegundaTela);
+
+                showToast(Name);
             }
         });
+    }
+
+    private void showToast(String text){
+        Toast.makeText(MainActivity.this, text, Toast.LENGTH_SHORT).show();
     }
 }
