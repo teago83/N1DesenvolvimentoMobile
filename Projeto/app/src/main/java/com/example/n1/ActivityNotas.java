d6057733b98668b996636df0a0a636b7e7672a69
teago83 <thyago.pereira@faculdadecesusc.edu.br>
2020-09-24 21:58:55 -0300

    Leves modificações.

--- Projeto/app/src/main/java/com/example/n1/ActivityNotas.java
+++ Projeto/app/src/main/java/com/example/n1/ActivityNotas.java
@@ -2,7 +2,10 @@ package com.example.n1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ActivityNotas extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class ActivityNotas extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notas);
+
+        Button BotaoTerceiraTela = (Button) findViewById(R.id.BotaoTerceiraTela);
+        BotaoTerceiraTela.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent IrTerceiraTela = new Intent(ActivityNotas.this, ActivityMedia.class);
+                // adicione o esquema de catar o nome da pessoa e passar para a intent
+                startActivity(IrTerceiraTela);
+            }
+        });
     }
 }
--- Projeto/app/src/main/java/com/example/n1/MainActivity.java
+++ Projeto/app/src/main/java/com/example/n1/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.n1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button BotaoSegundaTela = (Button) findViewById(R.id.BotaoSegundaTela);
+        BotaoSegundaTela.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent IrSegundaTela = new Intent(MainActivity.this, ActivityNotas.class);
+                // adicione o esquema de catar o nome da pessoa e passar para a intent
+                startActivity(IrSegundaTela);
+            }
+        });
     }
 }
