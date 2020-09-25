3f10dd84a30f40dedba85bc989cad35b578a14a3
teago83 <thyago.pereira@faculdadecesusc.edu.br>
2020-09-25 15:10:47 -0300

    App finalizado.

--- Projeto/app/src/main/java/com/example/n1/ActivityMedia.java
+++ Projeto/app/src/main/java/com/example/n1/ActivityMedia.java
@@ -4,23 +4,54 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 import android.widget.TextView;
 
 public class ActivityMedia extends AppCompatActivity {
+
+    WebView Gif;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_media);
 
+        Gif = findViewById(R.id.resultGif);
+        WebSettings ws = Gif.getSettings();
+        ws.setJavaScriptEnabled(true);
+        String FileLocation1 = "file:android_asset/parabains1.gif";
+        String FileLocation2 = "file:android_asset/F.gif";
+        String FileLocation3 = "file:android_asset/over9000.gif";
+
         TextView NotaNumero1 = (TextView) findViewById(R.id.nota1);
         TextView NotaNumero2 = (TextView) findViewById(R.id.nota2);
+        TextView Media = (TextView) findViewById(R.id.media);
+        TextView ParabensOuNen = (TextView) findViewById(R.id.parabensOuNen);
 
         Intent IncomingIntent = getIntent();
 
-        Integer IncomingNota1 = IncomingIntent.getIntExtra("Nota1", 0);
+        String IncomingNome = IncomingIntent.getStringExtra("Nome");
+        Float IncomingNota1 = IncomingIntent.getFloatExtra("Nota1", 0);
         NotaNumero1.setText(String.valueOf(IncomingNota1));
-        Integer IncomingNota2 = IncomingIntent.getIntExtra("Nota2", 0);
+        Float IncomingNota2 = IncomingIntent.getFloatExtra("Nota2", 0);
         NotaNumero2.setText(String.valueOf(IncomingNota2));
+        Float MediaFinal = (IncomingNota1 + IncomingNota2)/2;
+        Media.setText(String.valueOf(MediaFinal));
+
+        if (MediaFinal >= 6 || MediaFinal <= 10){
+            ParabensOuNen.setText("Você não reprovou, " + IncomingNome + "! Parabains!!!");
+            Gif.loadUrl(FileLocation1);
+        }
+        if (MediaFinal < 6){
+            ParabensOuNen.setText("Você reprovou, " + IncomingNome + "! F! :(");
+            Gif.loadUrl(FileLocation2);
+        }
+        if (MediaFinal > 10){
+            ParabensOuNen.setTextSize(21);
+            ParabensOuNen.setText("Espere um minuto, " + IncomingNome + "... sua média final foi mais de 10? Isso deve estar quebrado...");
+            Gif.loadUrl(FileLocation3);
+        }
+
     }
 }
--- Projeto/app/src/main/java/com/example/n1/ActivityNotas.java
+++ Projeto/app/src/main/java/com/example/n1/ActivityNotas.java
@@ -13,7 +13,7 @@ import android.widget.Toast;
 public class ActivityNotas extends AppCompatActivity {
 
     // Notas do usuário e EditTexts dos inputs das notas
-    Integer Nota1, Nota2;
+    Float Nota1, Nota2;
     EditText InputNota1, InputNota2;
 
     @Override
@@ -24,7 +24,7 @@ public class ActivityNotas extends AppCompatActivity {
         // Mensagem com o nome do usuário vindo da primeira tela.
         TextView MensagemNotas = (TextView) findViewById(R.id.MensagemNotas);
         Intent IncomingIntent = getIntent();
-        String IncomingName = IncomingIntent.getStringExtra("Username");
+        final String IncomingName = IncomingIntent.getStringExtra("Username");
         MensagemNotas.setText("Digite suas notas, " + IncomingName + ".");
 
         // Magias relacionadas com a intent que estará levando o usuário à terceira tela.
@@ -35,12 +35,13 @@ public class ActivityNotas extends AppCompatActivity {
         BotaoTerceiraTela.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Passe o nome do usuário também, não só as suas notas
-                Nota1 = Integer.valueOf(InputNota1.getText().toString()); // Essa função "Integer.valueOf()" é bem interessante e útil. Tente lembrar-se dela.
-                Nota2 = Integer.valueOf(InputNota2.getText().toString());
+                Nota1 = Float.valueOf(InputNota1.getText().toString()); // Essa função "Integer.valueOf()" é bem interessante e útil. Tente lembrar-se dela.
+                Nota2 = Float.valueOf(InputNota2.getText().toString());
+
                 Intent IrTerceiraTela = new Intent(ActivityNotas.this, ActivityMedia.class);
                 IrTerceiraTela.putExtra("Nota1", Nota1);
                 IrTerceiraTela.putExtra("Nota2", Nota2);
+                IrTerceiraTela.putExtra("Nome", IncomingName);
                 startActivity(IrTerceiraTela);
             }
         });
